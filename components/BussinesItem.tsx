892e83631bba18057c11c73a8039edebe3878170
Victor Salome <vsalome41@gmail.com>
2024-09-17 22:34:13 -0300

    feat: cosumindo a api e as imagens

--- components/BussinesItem.tsx
+++ components/BussinesItem.tsx
@@ -5,16 +5,19 @@ import { FaStar } from "react-icons/fa6";
 import { IDataMaps } from "./interfaces";
 
 interface BussinesitemProps {
+
     business: IDataMaps
 }
 
 export const BussinesItem = ({ business }: BussinesitemProps) => {
     console.log('business', business)
+
+    const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY
+    const photo_ref = business?.photos ? business?.photos[0]?.photo_reference : ''
     return (
         <div className="flex gap-3 p-3 border-b-[1px] border-cyan-300 mb-4 items-center">
-            <Image
-                src="/placeholder.jpg"
-                alt="placeholder"
+            <Image src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo_ref}&key=${GOOGLE_API_KEY}`}
+                alt="business-image"
                 width={90}
                 height={90}
                 className="rounded-xl object-cover h[100px] w-[100px]"
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    domains: ["maps.googleapis.com"],
+  },
+};
 
 export default nextConfig;
